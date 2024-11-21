75ee63dbf97ade52a881f66ed427ba33e498ac58
ahmedabdelrshed <104847521+ahmedabdelrshed@users.noreply.github.com>
2024-11-21 16:23:14 +0200

    footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,23 @@
 import  './footer.css'
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='flex'>
+       <ul className="flex">
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+          <li>
+            <a href="#">Articles</a>
+          </li>
+        </ul>
+        <p>&copy; 2024 My Portfolio. All rights reserved.</p>
+    </footer>
   )
 }
 
